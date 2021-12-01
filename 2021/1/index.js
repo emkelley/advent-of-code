a50a1775272d3d7a6cdda000eba1ce933c159f42
Eric Kelley <hello@ericmkelley.com>
2021-12-01 13:24:51 -0500

    2021 day 1

---
+++ 2021/1/index.js
@@ -0,0 +1,25 @@
+// ! https://adventofcode.com/2021/day/1
+
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt", { encoding: "utf-8" })
+  .split("\r\n")
+  .filter((x) => x)
+  .map(Number);
+
+// * Part 1
+let counter = 0;
+for (let i = 0; i < input.length; i++) {
+  if (input[i] > input[i - 1]) counter++;
+}
+console.log(counter);
+
+// * Part 2
+const count = input.reduce((acc, curr, i, src) => {
+  const previousSum = src[i - 1] + src[i] + src[i + 1];
+  const currentSum = src[i] + src[i + 1] + src[i + 2];
+  if (i === 0) return 0;
+  if (currentSum > previousSum) return acc + 1;
+  return acc;
+}, 0);
+console.log(count);
